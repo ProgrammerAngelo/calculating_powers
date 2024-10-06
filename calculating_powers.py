5ac3cd47dcedc2a26f9941b997df94fbae5446a3
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 04:23:11 -0400

    Final product

--- calculating_powers.py
+++ calculating_powers.py
@@ -1,15 +1,23 @@
 #make a method that calculate powers
 def calculating_powers(base, exponent):
-
+    # for base case
     if exponent == 0:
         return 1
+    # for recursive case
     else:
         return base * calculating_powers(base, exponent - 1)
-    
-base = int(input("Enter a base number: "))
-exponent = int(input("Enter a exponent: "))
 
+print("Hello user\n")
+
+#for inputing base number
+base = int(input("Please Enter base number: "))
+
+#for inputing exponent
+exponent = int(input("Please Enter exponent: "))
+
+#for calculating the result
 result = calculating_powers(base, exponent)
+#for printing the result
 print(f"The {base}^{exponent} is equal to {result}")
 
 calculating_powers(base, exponent)
