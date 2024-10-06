f621386f3094ea9636efe053b845d816c6336586
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 04:04:59 -0400

    added if,else function

--- calculating_powers.py
+++ calculating_powers.py
@@ -1,2 +1,9 @@
 #make a method that calculate powers
+def calculating_powers(base, exponent):
+
+    if exponent == 0:
+        return 1
+    else:
+        return base * calculating_powers(base, exponent - 1)
+calculating_powers()
 #add a recursive case
