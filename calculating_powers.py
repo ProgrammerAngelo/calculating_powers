c37351363f3873d3681eabfd57be51b2b3e28850
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 04:08:53 -0400

    added an input function for base number and exponent

--- calculating_powers.py
+++ calculating_powers.py
@@ -5,5 +5,9 @@ def calculating_powers(base, exponent):
         return 1
     else:
         return base * calculating_powers(base, exponent - 1)
+    
+base = float(input("Enter a base number: "))
+exponent = float(input("Enter a exponent: "))
+
 calculating_powers()
 #add a recursive case
